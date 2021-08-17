55682e9a0b34a1ab6c4856d2b2d581f49b921af6
robertomartinns@yahoo.com.br <robgit2021>
2021-08-17 20:25:31 -0300

    Aula 1 - Python

---
+++ Modulo1/Aula1.py
@@ -0,0 +1,17 @@
+#Aula 1 - Intrrodução ao Python
+#Fácil de aprender, Linguagem Versátil
+#Alguns topicos apresentados nas primeiras aulas
+#Instação, IDEs GoogleColab, PyCharm, ATOM etc
+print('Primeira Aula de Python')
+print ('Hello World!!!')
+print ('Ola galera!!!')
+
+print ('Estou adorando o bootcamp de Python')
+print ('Vou beber um copo d\'água e voltar a estudar Python')
+print ("Vou compartilhar com meus amigos sobre o bootcamp")
+
+var = 10 + 20
+print(var)
+
+num = 10 + 15
+print(num)
