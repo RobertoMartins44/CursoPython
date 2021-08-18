67e9393d026d28b3fe5a88e7bf91be6fa9356374
robertomartinns@yahoo.com.br <robgit2021>
2021-08-18 00:14:30 -0300

    Aula 1 - Python

--- Modulo1/Aula4.py
+++ Modulo1/Aula4.py
@@ -33,3 +33,43 @@ carros = ('Gol','Palio','X-terra','Hilux','Fusca')
 print(carros[0])
 #intervalo
 print(carros[1:5])
+
+times = ('SAO PAULO','PALMEIRAS','SANTOS','VASCO')
+print(times)
+
+#Juntas tuples
+
+tuples_junto = carros + times
+print(tuples_junto)
+
+#Sets------------------------------------------------------
+cidade = {'Manaus','Fortaleza','Natal','Rio de Janeiro'}
+cidade2 = {'Curitiba','Porto Alegre','Salvador','Aracaju'}
+
+print(type(cidade))
+print(cidade)
+print('Manaus' in cidade)
+
+ling_pro = {'Python','Java','C++','Cobol'}
+print(ling_pro)
+
+#Adicionar um item no Set
+
+ling_pro.add('PHP')
+print(ling_pro)
+
+#Juntar Sets
+
+cidade.update(cidade2)
+print(cidade)
+
+#Dictinary-------------------------------------------------
+
+cod_uf  = {21 : 'Maranhão',
+           22 : 'Manaus',
+           23 : 'Tocantins',
+           24 : 'São Paulo',
+           25 : 'Belo Horizonte',
+           26 : 'Brasilia',
+           27 : 'Rio de Janeiro'}
+print(cod_uf)
