dc7ec15b1299ca72705873d3ed6a5a296bba1850
robertomartinns@yahoo.com.br <robgit2021>
2021-08-19 00:01:12 -0300

    Aula 1 - Python

--- Modulo1/Aula6.py
+++ Modulo1/Aula6.py
@@ -1,17 +1,19 @@
 #Funções
-import pandas as pd
-from geopy import geocode
-from math import pi, sqrt
 
-def calc_area (point1,point2):
-  x1,y1 = point1
-  x2,y2 = point2
-  return (x1*y2 - y1*x2) / 2
 
 
 #lambda função anonima
 x = 5
 y = 8
-soma = lambda x, y: x + y
+soma = lambda x,y: x + y
+print(soma)
 
+#import pandas as pd
+#from geopy import geocode
+#from math import pi, sqrt
+
+#def calc_area (point1,point2):
+#  x1,y1 = point1
+#  x2,y2 = point2
+#  return (x1*y2 - y1*x2) / 2
 
