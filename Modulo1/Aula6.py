6757de94cf9056b8a98256dad80aee52f0b690f2
robertomartinns@yahoo.com.br <robgit2021>
2021-08-19 00:06:33 -0300

    Aula 1 - Python

--- Modulo1/Aula6.py
+++ Modulo1/Aula6.py
@@ -1,5 +1,34 @@
 #Funções
 
+def hello (meu_nome):
+  print ('Olá',meu_nome)
+
+hello('Roberto')
+
+def hello1 (meu_name,idade):
+  print('Olá',meu_name, ', Sua Idade é: ',idade)
+
+hello1('Roberto',44)
+
+def calcular_pagamento (qtd_horas,valor_hora):
+  horas = float(qtd_horas)
+  taxa = float(valor_hora)
+  if horas < 40:
+    salario = horas * taxa
+  else:
+    h_excd = horas - 40
+    salario = 40 * taxa + (h_excd * (1.5 * taxa))
+    return salario
+
+str_horas = input ('Digite as horas: ')
+str_taxa  = input ('Digite a taxa: ')
+total_salario = calcular_pagamento (str_horas,str_taxa)
+print ('O valor de seus rendimentos é R$ :',total_salario)
+
+
+
+
+
 
 
 #lambda função anonima
